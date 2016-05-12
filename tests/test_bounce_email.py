ffa0e688a15d20c214a5dbc58df43e8fc0e368d2
Ronnie Beltran <rbbeltran.09@gmail.com>
2016-05-12 16:54:38 +0800

    Setup unit tests for is_bounced method

--- tests/test_bounce_email.py
+++ tests/test_bounce_email.py
@@ -9,8 +9,19 @@ class BounceEmailTest(unittest.TestCase):
     def setUp(self):
         self.bounce1 = constants.BOUNCE_EMAIL1
         self.bounce2 = constants.BOUNCE_EMAIL2
-        self.normal_email = constants.NORMAL_EMAIL1
+        self.notbounce = constants.NORMAL_EMAIL1
 
-    def test_init(self):
+    def test_is_bounced(self):
         bounce = bounce_email.BounceEmail(self.bounce1)
-        self.assertEqual(bounce.is_bounce(), False)
+        self.assertNotEqual(bounce, None)
+        self.assertEqual(bounce.is_bounced(), True)
+
+    def test_is_bounced2(self):
+        bounce = bounce_email.BounceEmail(self.bounce2)
+        self.assertNotEqual(bounce, None)
+        self.assertEqual(bounce.is_bounced(), True)
+
+    def test_not_bounced(self):
+        bounce = bounce_email.BounceEmail(self.notbounce)
+        self.assertNotEqual(bounce, None)
+        self.assertEqual(bounce.is_bounced(), False)
