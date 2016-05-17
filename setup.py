7365f1d3c8eeef32022c9f467d2d79c735b9e004
Ronnie Beltran <rbbeltran.09@gmail.com>
2016-05-17 17:17:43 +0800

    Setup bounce_email package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='bounce_email',
+    version='0.1dev',
+    packages='bounce_email',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
