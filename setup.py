49c6fb7577e2aa9c95a7b58f316ddef720b20456
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-01-26 10:41:46 +0800

    Update setup, use setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='bounce_email',
-    version='0.1dev',
-    packages=['bounce_email'],
-    license='MIT',
-    long_description=open('README.md').read(),
+    version='0.0.1',
+    description=open('README.md').read(),
+    url='http://github.com/ronbeltran/bounce_email',
     author='Ronnie Beltran',
     author_email='rbbeltran.09@gmail.com',
+    license='MIT',
+    packages=['bounce_email'],
+    zip_safe=False,
 )
