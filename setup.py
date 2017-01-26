e5741cafa35761b32f7e91c39d0d586fc59261c9
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-01-26 10:57:00 +0800

    Update package setup for pypi

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 setup(
     name='bounce_email',
     version='0.0.1',
-    description=open('README.md').read(),
+    description='Port of Ruby version bounce_email to Python to detect bounced emails',
+    long_description=open('README.md').read(),
+    keywords='gmail detect bounce email',
     url='http://github.com/ronbeltran/bounce_email',
     author='Ronnie Beltran',
     author_email='rbbeltran.09@gmail.com',
     license='MIT',
     packages=['bounce_email'],
+    include_package_data=True,
     zip_safe=False,
 )
